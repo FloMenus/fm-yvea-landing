ddcc160146e913a7828a2140cb8bd3a9fd2caf01
FloMenus <florent.mns@gmail.com>
2023-09-13 12:42:39 +0200

    fix 1 for SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="robots" content="all" />
       </Head>
       <body className={nunito_sans.className}>
         <Navbar />
