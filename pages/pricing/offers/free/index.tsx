d4dafa850120746391aa96b156ea8820a97bb947
FloMenus <florent.mns@gmail.com>
2023-09-26 18:48:10 +0200

    finished

--- pages/pricing/offers/free/index.tsx
+++ pages/pricing/offers/free/index.tsx
@@ -2,6 +2,7 @@ import styles from "./style.module.css";
 
 import Head from "next/head";
 import Link from "next/link";
+import Image from "next/image";
 import { useTranslation } from "react-i18next";
 
 export default function FreeOffer() {
@@ -19,86 +20,118 @@ export default function FreeOffer() {
       <div className={styles.main_container}>
         <div className={styles.header_section}>
           <h1 className={styles.title}>
-            {t("YVEA Free : Découvrez l'efficacité, même en mode freemium.")}
+            <span className={styles.title_bold}>{t("YVEA Free")}</span>
+            {t(" : Découvrez l'efficacité, même en mode freemium.")}
           </h1>
           <p className={styles.subtitle}>
             {t("Avec YVEA, simplicité ne rime pas avec limitation.")}
           </p>
+          <Image
+            className={styles.header_cover}
+            src="/medias/free.png"
+            width={300}
+            height={300}
+            alt="free"
+          />
         </div>
-        <h2 className={styles.second_title}>
-          {t("Toujours à vos côtés, même sans dépenser un centime.")}
-        </h2>
-        <ul className={styles.list_number}>
-          <li className={styles.list_item}>
-            <p className={styles.list_description}>
-              {t(
-                "1 Vérification Instantanée: Assurez-vous à tout moment si vos produits nécessitent un contrôle PVOC"
-              )}
-            </p>
-          </li>
-          <li className={styles.list_item}>
-            <p className={styles.list_description}>
-              {t(
-                "2 Estimation Précise : Anticipez vos dépenses en simulant le coût du contrôle PVoC au centime près."
-              )}
-            </p>
-          </li>
-        </ul>
+        <div className={styles.content}>
+          <div className={styles.section}>
+            <h2 className={styles.second_title}>
+              {t("Toujours à vos côtés, même sans dépenser un centime.")}
+            </h2>
+            <ul className={styles.list_number}>
+              <li className={styles.square}>
+                <p className={styles.list_description}>
+                  {t(
+                    "1 | Vérification Instantanée: Assurez-vous à tout moment si vos produits nécessitent un contrôle PVOC"
+                  )}
+                </p>
+              </li>
+              <li className={styles.square}>
+                <p className={styles.list_description}>
+                  {t(
+                    "2 | Estimation Précise : Anticipez vos dépenses en simulant le coût du contrôle PVoC au centime près."
+                  )}
+                </p>
+              </li>
+            </ul>
+          </div>
 
-        <h2 className={styles.second_title}>
-          {t("Faites-nous confiance, sans engagement.")}
-        </h2>
-        <p className={styles.paragraph}>
-          {t(
-            "Nous savons combien il est essentiel de construire une relation de confiance. C'est pourquoi nous vous proposons de découvrir nos services sans frais pour votre premier dossier :"
-          )}
-        </p>
-        <ul className={styles.list}>
-          <li className={styles.list_item}>
-            <p className={styles.list_description}>
-              <span className={styles.item_bold}>{t("Rapidité")}</span>
-              {t(" : Constituez un dossier complet en un éclair.")}
-            </p>
-          </li>
-          <li className={styles.list_item}>
-            <p className={styles.list_description}>
-              <span className={styles.item_bold}>{t("Accompagnement")}</span>
+          <div className={styles.section}>
+            <h2 className={styles.second_title}>
+              {t("Faites-nous confiance, sans engagement.")}
+            </h2>
+            <p className={styles.paragraph}>
               {t(
-                " : Bénéficiez d'un expert dédié pour suivre votre dossier de A à Z."
+                "Nous savons combien il est essentiel de construire une relation de confiance. C'est pourquoi nous vous proposons de découvrir nos services sans frais pour votre premier dossier :"
               )}
             </p>
-          </li>
-          <li className={styles.list_item}>
-            <p className={styles.list_description}>
-              <span className={styles.item_bold}>{t("Priorité")}</span>
+            <ul className={styles.list}>
+              <li className={styles.list_item}>
+                <span className={styles.item_bold}>{t("• Rapidité")}</span>
+                <p className={styles.list_description}>
+                  {t(" : Constituez un dossier complet en un éclair.")}
+                </p>
+              </li>
+              <li className={styles.list_item}>
+                <p className={styles.list_description}>
+                  <span className={styles.item_bold}>
+                    {t("• Accompagnement")}
+                  </span>
+                  {t(
+                    " : Bénéficiez d'un expert dédié pour suivre votre dossier de A à Z."
+                  )}
+                </p>
+              </li>
+              <li className={styles.list_item}>
+                <p className={styles.list_description}>
+                  <span className={styles.item_bold}>{t("• Priorité")}</span>
+                  {t(
+                    " : Avec YVEA, votre dossier est toujours traité en priorité."
+                  )}
+                </p>
+              </li>
+            </ul>
+          </div>
+
+          <div className={styles.section}>
+            <h2 className={styles.second_title}>
+              {t("Qualité, sans se ruiner.")}
+            </h2>
+            <p className={styles.paragraph}>
               {t(
-                " : Avec YVEA, votre dossier est toujours traité en priorité."
+                "Nous mettons un point d'honneur à offrir le meilleur, quel que soit votre budget. Pour seulement "
+              )}
+              <span className={styles.price_bold}>{t("39,95€ HT")}</span>
+              {t(" par dossier, bénéficiez d'une expertise inégalée.")}
+            </p>
+          </div>
+
+          <div className={styles.section}>
+            <h2 className={styles.second_title}>{t("Option Express Gabon")}</h2>
+            <p className={styles.paragraph}>
+              {t(
+                "Besoin d'accélérer les choses pour le Gabon ? Avec cette option, obtenez votre certificat en moins de 72h. Après vérification par votre interlocuteur dédié, profitez de cette vitesse accrue pour seulement +19,95€ par dossier."
               )}
             </p>
-          </li>
-        </ul>
-        <h2 className={styles.second_title}>{t("Qualité, sans se ruiner.")}</h2>
-        <p className={styles.paragraph}>
-          {t(
-            "Nous mettons un point d'honneur à offrir le meilleur, quel que soit votre budget. Pour seulement 39,95€ HT par dossier, bénéficiez d'une expertise inégalée."
-          )}
-        </p>
-        <h2 className={styles.second_title}>{t("Option Express Gabon")}</h2>
-        <p className={styles.paragraph}>
-          {t(
-            "Besoin d'accélérer les choses pour le Gabon ? Avec cette option, obtenez votre certificat en moins de 72h. Après vérification par votre interlocuteur dédié, profitez de cette vitesse accrue pour seulement +19,95€ par dossier."
-          )}
-        </p>
-        <h2 className={styles.second_title}>{t("Envie de plus ?")}</h2>
-        <p className={styles.paragraph}>
-          {t("Passez à la vitesse supérieure avec notre")}
-          <Link className={styles.link} href="/pricing/offers/pro">
-            {t(" offre Pro")}
-          </Link>
-          {t(
-            ". Plus de fonctionnalités, plus de flexibilité, pour une expérience YVEA optimisée."
-          )}
-        </p>
+          </div>
+
+          <div className={styles.section}>
+            <h2 className={styles.second_title}>{t("Envie de plus ?")}</h2>
+            <p className={styles.paragraph}>
+              {t("Passez à la vitesse supérieure avec notre ")}
+              <Link
+                className={`${styles.link} ${styles.offer}`}
+                href="/pricing/offers/pro"
+              >
+                {t("offre Pro")}
+              </Link>
+              {t(
+                ". Plus de fonctionnalités, plus de flexibilité, pour une expérience YVEA optimisée."
+              )}
+            </p>
+          </div>
+        </div>
       </div>
     </>
   );
--- pages/pricing/offers/pro/index.tsx
+++ pages/pricing/offers/pro/index.tsx
@@ -2,6 +2,7 @@ import styles from "./style.module.css";
 
 import Head from "next/head";
 import Link from "next/link";
+import Image from "next/image";
 import { useTranslation } from "react-i18next";
 
 export default function ProOffer() {
@@ -19,126 +20,168 @@ export default function ProOffer() {
       <div className={styles.main_container}>
         <div className={styles.header_section}>
           <h1 className={styles.title}>
-            {t("YVEA PRO : L'excellence à portée de main.")}
+            <span className={styles.title_bold}>{t("YVEA Pro")}</span>
+            {t(" : L'excellence à portée de main.")}
           </h1>
           <p className={styles.subtitle}>
             {t(
               "Pensée pour les entreprises exigeantes, l'offre Pro apporte une dimension supérieure à votre expérience d'exportation."
             )}
           </p>
+          <Image
+            className={styles.header_cover}
+            src="/medias/premium.png"
+            width={300}
+            height={300}
+            alt="premium"
+          />
         </div>
-
-        <h2 className={styles.second_title}>
-          {t("Maximisez chaque instant.")}
-        </h2>
-        <p className={styles.paragraph}>
-          {t(
-            "L'offre Pro est conçue pour les professionnels souhaitant optimiser chaque étape de leur processus d'exportation :"
-          )}
-        </p>
-        <ul className={styles.list}>
-          <li className={styles.list_item}>
-            <p className={styles.list_description}>
-              <span className={styles.item_bold}>{t("Option Brouillon")}</span>
+        <div className={styles.content}>
+          <div className={styles.section}>
+            <h2 className={styles.second_title}>
+              {t("Maximisez chaque instant.")}
+            </h2>
+            <p className={styles.paragraph}>
               {t(
-                " : Commencez, mettez en pause, puis reprenez la constitution de votre dossier quand vous le souhaitez."
+                "L'offre Pro est conçue pour les professionnels souhaitant optimiser chaque étape de leur processus d'exportation :"
               )}
             </p>
-          </li>
-          <li className={styles.list_item}>
-            <p className={styles.list_description}>
-              <span className={styles.item_bold}>
-                {t("Réutilisation des données")}
-              </span>
-              {t(" : Fini les saisies répétitives. YVEA mémorise pour vous.")}
-            </p>
-          </li>
-          <li className={styles.list_item}>
-            <p className={styles.list_description}>
-              <span className={styles.item_bold}>
-                {t("Gestion documentaire automatisée")}
-              </span>
+            <ul className={styles.list_number}>
+              <li className={styles.square}>
+                <p className={styles.list_description}>
+                  <span className={styles.item_bold}>
+                    {t("Option Brouillon")}
+                  </span>
+                  <br />
+                  <br />
+                  {t(
+                    "Commencez, mettez en pause, puis reprenez la constitution de votre dossier quand vous le souhaitez."
+                  )}
+                </p>
+              </li>
+              <li className={styles.square}>
+                <p className={styles.list_description}>
+                  <span className={styles.item_bold}>
+                    {t("Réutilisation des données")}
+                  </span>
+                  <br />
+                  <br />
+                  {t("Fini les saisies répétitives. YVEA mémorise pour vous.")}
+                </p>
+              </li>
+              <li className={styles.square}>
+                <p className={styles.list_description}>
+                  <span className={styles.item_bold}>
+                    {t("Gestion documentaire automatisée")}
+                  </span>
+                  <br />
+                  <br />
+                  {t(
+                    "Transformez votre gestion documentaire en expérience digitale."
+                  )}
+                </p>
+              </li>
+            </ul>
+          </div>
+
+          <div className={styles.section}>
+            <h2 className={styles.second_title}>
+              {t("Tarifs avantageux pour les professionnels.")}
+            </h2>
+            <p className={styles.paragraph}>
               {t(
-                " : Transformez votre gestion documentaire en expérience digitale"
+                "Avec l'offre Pro, les frais d'accompagnement par dossier PVoC sont réduits à 35,00€. Un investissement minimal pour une qualité de service maximale."
               )}
             </p>
-          </li>
-        </ul>
+          </div>
 
-        <h2 className={styles.second_title}>
-          {t("Tarifs avantageux pour les professionnels.")}
-        </h2>
-        <p className={styles.paragraph}>
-          {t(
-            "Avec l'offre Pro, les frais d'accompagnement par dossier PVoC sont réduits à 35,00€. Un investissement minimal pour une qualité de service maximale."
-          )}
-        </p>
-
-        <h2 className={styles.second_title}>
-          {t("Réduisez les délais sans compromettre la qualité.")}
-        </h2>
-        <p className={styles.paragraph}>
-          {t("Chaque seconde compte. Avec Pro :")}
-        </p>
-        <ul className={styles.list}>
-          <li className={styles.list_item}>
-            <p className={styles.list_description}>
-              <span className={styles.item_bold}>
-                {t("Option Express Gabon Incluse")}
-              </span>
-              {t(" : Votre certificat en moins de 72h.")}
+          <div className={styles.section}>
+            <h2 className={styles.second_title}>
+              {t("Réduisez les délais sans compromettre la qualité.")}
+            </h2>
+            <p className={styles.paragraph}>
+              {t("Chaque seconde compte. Avec Pro :")}
             </p>
-          </li>
-          <li className={styles.list_item}>
-            <p className={styles.list_description}>
-              <span className={styles.item_bold}>
-                {t("Frais d'accompagnement pour l'Arabie Saoudite inclus")}
-              </span>
-              {t(" :  Nous gérons, vous progressez.")}
+            <ul className={styles.list_number}>
+              <li className={`${styles.square} ${styles.square_bis}`}>
+                <p className={styles.list_description}>
+                  <span className={styles.item_bold}>
+                    {t("Option Express Gabon Incluse")}
+                  </span>
+                  <br />
+                  <br />
+                  {t("Votre certificat en moins de 72h.")}
+                </p>
+              </li>
+              <li className={`${styles.square} ${styles.square_bis}`}>
+                <p className={styles.list_description}>
+                  <span className={styles.item_bold}>
+                    {t("Frais d'accompagnement pour l'Arabie Saoudite inclus")}
+                  </span>
+                  <br />
+                  <br />
+                  {t("Nous gérons, vous progressez.")}
+                </p>
+              </li>
+            </ul>
+          </div>
+
+          <div className={styles.section}>
+            <h2 className={styles.second_title}>
+              {t("Profitez d'une tarification avantageuse.")}
+            </h2>
+            <p className={styles.paragraph}>
+              {t("Accédez à l'offre Pro pour seulement ")}
+              <span className={styles.price_bold}>{t("12,95€")}</span>
+              {t(
+                "/utilisateur par mois (facturé annuellement). Maximisez vos bénéfices tout en maîtrisant vos coûts."
+              )}
             </p>
-          </li>
-        </ul>
+          </div>
 
-        <h2 className={styles.second_title}>
-          {t("Profitez d'une tarification avantageuse.")}
-        </h2>
-        <p className={styles.paragraph}>
-          {t(
-            "Accédez à l'offre Pro pour seulement 12,95€/utilisateur par mois (facturé annuellement). Maximisez vos bénéfices tout en maîtrisant vos coûts."
-          )}
-        </p>
+          <div className={styles.section}>
+            <h2 className={styles.second_title}>
+              {t("Des bénéfices exclusifs.")}
+            </h2>
+            <p className={styles.paragraph}>
+              {t(
+                "Bénéficiez de remises spéciales sur les services de nos partenaires, et explorez de nouvelles façons de grandir."
+              )}
+            </p>
+          </div>
 
-        <h2 className={styles.second_title}>{t("Des bénéfices exclusifs.")}</h2>
-        <p className={styles.paragraph}>
-          {t(
-            "Bénéficiez de remises spéciales sur les services de nos partenaires, et explorez de nouvelles façons de grandir."
-          )}
-        </p>
+          <div className={styles.section}>
+            <h2 className={styles.second_title}>
+              {t("Intéressé(e) par YVEA Pro ?")}
+            </h2>
+            <p className={styles.paragraph}>
+              {t("L'accès à l'offre Pro nécessite une validation préalable. ")}
+              <Link
+                className={styles.link_form}
+                href="https://docs.google.com/forms/u/1/d/e/1FAIpQLSemoVIH-3smSe3CnBAoQvMzHLSzsl8Sh69O7Ajv1ofWq6fqyA/viewform"
+              >
+                {t("Remplissez notre formulaire")}
+              </Link>
+              {t(" et nous reviendrons vers vous rapidement.")}
+            </p>
+          </div>
 
-        <h2 className={styles.second_title}>
-          {t("Intéressé(e) par YVEA Pro ?")}
-        </h2>
-        <p className={styles.paragraph}>
-          {t("L'accès à l'offre Pro nécessite une validation préalable.")}
-          <Link
-            className={styles.link}
-            href="https://docs.google.com/forms/u/1/d/e/1FAIpQLSemoVIH-3smSe3CnBAoQvMzHLSzsl8Sh69O7Ajv1ofWq6fqyA/viewform"
-          >
-            {t(" Remplissez notre formulaire")}
-          </Link>
-          {t(" et nous reviendrons vers vous rapidement.")}
-        </p>
-
-        <h2 className={styles.second_title}>
-          {t("Vous souhaitez commencer en douceur ?")}
-        </h2>
-        <p className={styles.paragraph}>
-          {t("Découvrez")}
-          <Link className={styles.link} href="/pricing/offers/free">
-            {t(" l'offre Free ")}
-          </Link>
-          {t("pour une première immersion dans l'univers YVEA.")}
-        </p>
+          <div className={styles.section}>
+            <h2 className={styles.second_title}>
+              {t("Vous souhaitez commencer en douceur ?")}
+            </h2>
+            <p className={styles.paragraph}>
+              {t("Découvrez ")}
+              <Link
+                className={`${styles.link} ${styles.offer}`}
+                href="/pricing/offers/free"
+              >
+                {t("l'offre Free")}
+              </Link>
+              {t(" pour une première immersion dans l'univers YVEA.")}
+            </p>
+          </div>
+        </div>
       </div>
     </>
   );
